0f10a89191ae76e7464df92419db3fd7de8ce438
Arafathislam <islamarafath315@gmail.com>
2023-05-09 01:33:38 +0600

    v5

--- mui/src/App.js
+++ mui/src/App.js
@@ -1,9 +1,14 @@
 
+import Button from '@mui/material/Button';
 
 function App() {
   return (
  <>
   <h1>Arafath Islam</h1>
+  <Button color="primary">Click ME</Button><br/>
+  <Button color="warning">Click ME</Button><br/>
+  <Button color="warning" variant="contained">Click ME</Button><br/>
+  <Button variant="outlined">Click ME</Button>
  </>
   );
 }
